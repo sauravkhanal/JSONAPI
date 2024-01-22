7fed698097488c3c45720637571370a5252a8fb2
sauravkhanal <sauravkhanal635@gmail.com>
2024-01-22 22:58:05 +0545

    modifies json keys

--- src/Components/BodyRight.jsx
+++ src/Components/BodyRight.jsx
@@ -1,8 +1,14 @@
 import React, { useState } from 'react'
+import handleRequest from '../modules/handleRequest';
 
 function BodyRight() {
-    const [Name, setName] = useState('');
-    const [JSONContent, setJSONContent] = useState('');
+    const [name, setName] = useState('');
+    const [json, setJSONContent] = useState('');
+
+    const handleSubmit = async({name, json}) => {
+        return null
+    }
+
     return (
         <div className='bg-gray-500 rounded-l-none p-2 sm:p-5 md:p-7 lg:p-10 w-full h-full flex justify-center items-center'>
 
@@ -13,25 +19,25 @@ function BodyRight() {
                 className=' flex flex-col w-full h-full space-y-3 '
             >
 
-                <label htmlFor='Name' className='flex flex-col text-lg font-medium'>Name
-                    <input type='text' placeholder="Name" id='Name' minLength={5} name='Name' required
+                <label htmlFor='name' className='flex flex-col text-lg font-medium'>Name
+                    <input type='text' placeholder="name" id='name' minLength={5} name='name' required
                         onInput={(event) => { setName(event.target.value) }}
                         className='rounded-md border border-solid  border- p-1 font-normal'
                         autoComplete='off'
                     />
                 </label>
-                <label htmlFor='JsonContent' className='flex flex-col font-medium'>JSON
+                <label htmlFor='json' className='flex flex-col font-medium'>JSON
                     <textarea
                         required
                         rows={12}
-                        JSONName='JsonContent'
+                        name='json'
                         placeholder='{"Choose Kindness":"❤️"}'
                         onInput={(event) => { setJSONContent(event.target.value) }}
-                        id='JsonContent'
+                        id='json'
                         className='resize-none rounded-md p-1 font-normal'
                     />
                 </label>
-                <button type='submit' 
+                <button onClick={handleSubmit} 
                     className='rounded-md bg-black text-white self-center px-5 py-1 active:bg-gray-700 text-xl '
                 >Submit</button>
             </form>
