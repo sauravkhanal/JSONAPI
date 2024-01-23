719b5ba77f5f3449b4683238fc54174b564ac6e0
sauravkhanal <sauravkhanal635@gmail.com>
2024-01-23 16:34:37 +0545

    adds dialog modal , appears on submit

--- src/Components/BodyRight.jsx
+++ src/Components/BodyRight.jsx
@@ -1,12 +1,31 @@
-import React, { useState } from 'react'
+import React, { useRef, useState } from 'react'
 import handleRequest from '../modules/handleRequest';
+import { FaRegCopy } from "react-icons/fa";
 
 function BodyRight() {
     const [name, setName] = useState('');
     const [json, setJSONContent] = useState('');
+    const [response, setResponse] = useState('');
+    const [responseOk, setResponseOK] = useState(false)
+    const dialogs = useRef();
+    const urlRef = useRef();
 
-    const handleSubmit = async({name, json}) => {
-        return null
+    const copyToClipboard = async () => {
+        navigator.clipboard.writeText(urlRef.current.innerText)
+            .then(() => {
+                console.log("URL copied to clipboard")
+                setResponse("URL_successfully_copied_to_clipboard")
+            })
+            .catch(error => console.error('Unable to copy to clipboard: ', error));
+        // dialogs.current.close()
+    }
+
+    const handleSubmit = async () => {
+        const { response, responseOk } = await handleRequest({ name, json })
+        setResponse(response)
+        setResponseOK(responseOk)
+
+        dialogs.current?.showModal();
     }
 
     return (
@@ -37,10 +56,35 @@ function BodyRight() {
                         className='resize-none rounded-md p-1 font-normal'
                     />
                 </label>
-                <button onClick={handleSubmit} 
+                {/* <p className='text-black text-center'>{response}</p> */}
+                <button onClick={handleSubmit}
                     className='rounded-md bg-black text-white self-center px-5 py-1 active:bg-gray-700 text-xl '
                 >Submit</button>
             </form>
+
+            <dialog ref={dialogs} className='overflow-hidden bg-transparent backdrop-blur-3xl'>
+                <div className='rounded-md bg-amber-100 min-w-96 min-h-48 border border-black border-solid flex flex-col justify-center items-center overflow-hidden p-5'>
+
+
+                    {responseOk ?
+                        <p
+                            ref={urlRef}
+                            onClick={copyToClipboard}
+                            className='px-2 py-2 border bg-gray-900 text-white rounded-lg flex items-center gap-5'
+                        >{response.split(' ').pop()}<FaRegCopy size={30} className='active:text-green-400' /></p>
+                        :
+                        <p className='flex-grow'>{response}</p>
+                    }
+
+
+                    <form method="dialog" className='my-3' >
+                        <button className='rounded-md bg-black text-white self-center px-5 py-1 active:bg-gray-700 text-xl '>OK</button>
+                    </form>
+                </div>
+            </dialog>
+
+
+
         </div>
     )
 }
