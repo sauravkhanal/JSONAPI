cc69210ad207f41f64083de09361acc176e54331
sauravkhanal <sauravkhanal635@gmail.com>
2024-05-10 15:54:02 +0545

    update sample json url

--- src/modules/getSampleJson.js
+++ src/modules/getSampleJson.js
@@ -1,9 +1,8 @@
 async function getSampleJson() {
     try {
-        const response = await fetch("https://json.gorkhacloud.com/api/SampleJSON/GetDistrictsByDevelopmentRegion");
-        const res = await response.json();
-  
-        return res.formattedString
+        const response = await fetch("https://json.khanalsaurav.com.np/json/sample");
+        const json = await response.json();
+        return json
 
     } catch (error) {
         console.error("Error fetching data:", error);
